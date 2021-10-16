722aabc284e80071eae15c275e654b66ac18d1ca
nyan101 <k.minchul95@gmail.com>
2021-10-16 15:49:24 +0900

    Board 프론트 추가. Announcement 기능을 기반으로 announcement->post / contest->problem 으로 대체

--- OnlineJudgeFE/src/i18n/index.js
+++ OnlineJudgeFE/src/i18n/index.js
@@ -1,20 +1,14 @@
 // element UI
 import elenUS from 'element-ui/lib/locale/lang/en';
-import elzhCN from 'element-ui/lib/locale/lang/zh-CN';
-import elzhTW from 'element-ui/lib/locale/lang/zh-TW';
 // ivew UI
 import ivenUS from 'iview/dist/locale/en-US';
-import ivzhCN from 'iview/dist/locale/zh-CN';
-import ivzhTW from 'iview/dist/locale/zh-TW';
 import Vue from 'vue';
 import VueI18n from 'vue-i18n';
 
 Vue.use(VueI18n);
 
 const languages = [
-  { value: 'en-US', label: 'English', iv: ivenUS, el: elenUS },
-  { value: 'zh-CN', label: '简体中文', iv: ivzhCN, el: elzhCN },
-  { value: 'zh-TW', label: '繁體中文', iv: ivzhTW, el: elzhTW },
+  { value: 'en-US', label: 'English', iv: ivenUS, el: elenUS }
 ];
 const messages = {};
 
---
+++ OnlineJudgeFE/src/pages/oj/views/general/Board.vue
@@ -0,0 +1,177 @@
+<template>
+  <Panel shadow :padding="10">
+    <div slot="title">
+      {{ title }}
+    </div>
+    <div slot="extra">
+      <Button v-if="listVisible" type="info" @click="init" :loading="btnLoading">
+        {{ $t('m.Refresh') }}
+      </Button>
+      <Button v-else type="ghost" icon="ios-undo" @click="goBack">{{ $t('m.Back') }}</Button>
+    </div>
+
+    <transition-group name="post-animate" mode="in-out">
+      <div class="no-post" v-if="!posts.length" key="no-post">
+        <p>{{ $t('m.No_Announcements') }}</p>
+      </div>
+      <template v-if="listVisible">
+        <ul class="posts-container" key="list">
+          <li v-for="post in posts" :key="post.title">
+            <div class="flex-container">
+              <div class="title"><a class="entry" @click="goPost(post)">
+                {{ post.title }}</a></div>
+              <div class="date">{{ post.create_time | localtime }}</div>
+              <div class="creator"> {{ $t('m.By') }} {{ post.created_by.username }}</div>
+            </div>
+          </li>
+        </ul>
+        <Pagination v-if="!isProblem"
+                    key="page"
+                    :total="total"
+                    :page-size="limit"
+                    @on-change="getPostList">
+        </Pagination>
+      </template>
+
+      <template v-else>
+        <div v-katex v-html="post.content" key="content" class="content-container markdown-body"></div>
+      </template>
+    </transition-group>
+  </Panel>
+</template>
+
+<script>
+import api from '@oj/api';
+import Pagination from '@oj/components/Pagination';
+
+export default {
+  name: 'Board',
+  components: {
+    Pagination,
+  },
+  data() {
+    return {
+      limit: 10,
+      total: 10,
+      btnLoading: false,
+      posts: [],
+      post: '',
+      listVisible: true,
+    };
+  },
+  mounted() {
+    this.init();
+  },
+  methods: {
+    init() {
+      if (this.isProblem) {
+        this.getProblemPostList();
+      } else {
+        this.getPostList();
+      }
+    },
+    getPostList(page = 1) {
+      this.btnLoading = true;
+      api.getPostList((page - 1) * this.limit, this.limit).then(res => {
+        this.btnLoading = false;
+        this.posts = res.data.data.results;
+        this.total = res.data.data.total;
+      }, () => {
+        this.btnLoading = false;
+      });
+    },
+    getProblemPostList() {
+      this.btnLoading = true;
+      api.getProblemPostList(this.$route.params.problemID).then(res => {
+        this.btnLoading = false;
+        this.posts = res.data.data;
+      }, () => {
+        this.btnLoading = false;
+      });
+    },
+    goPost(post) {
+      this.post = post;
+      this.listVisible = false;
+    },
+    goBack() {
+      this.listVisible = true;
+      this.post = '';
+    },
+  },
+  computed: {
+    title() {
+      if (this.listVisible) {
+        return this.isProblem ? this.$i18n.t('m.Contest_Announcements') : this.$i18n.t('m.Announcements');
+      } else {
+        return this.post.title;
+      }
+    },
+    isProblem() {
+      return !!this.$route.params.problemID;
+    },
+  },
+};
+</script>
+
+<style scoped lang="less">
+.posts-container {
+  margin-top: -10px;
+  margin-bottom: 10px;
+
+  li {
+    padding-top: 15px;
+    list-style: none;
+    padding-bottom: 15px;
+    margin-left: 20px;
+    font-size: 16px;
+    border-bottom: 1px solid rgba(187, 187, 187, 0.5);
+
+    &:last-child {
+      border-bottom: none;
+    }
+
+    .flex-container {
+      .title {
+        flex: 1 1;
+        text-align: left;
+        padding-left: 10px;
+
+        a.entry {
+          color: #495060;
+
+          &:hover {
+            color: #2d8cf0;
+            border-bottom: 1px solid #2d8cf0;
+          }
+        }
+      }
+
+      .creator {
+        flex: none;
+        width: 200px;
+        text-align: center;
+      }
+
+      .date {
+        flex: none;
+        width: 200px;
+        text-align: center;
+      }
+    }
+  }
+}
+
+.content-container {
+  padding: 0 20px 20px 20px;
+}
+
+.no-post {
+  text-align: center;
+  font-size: 16px;
+}
+
+changeLocale
+.post-animate-enter-active {
+  animation: fadeIn 1s;
+}
+</style>
