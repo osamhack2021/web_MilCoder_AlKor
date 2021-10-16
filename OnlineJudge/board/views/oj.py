faea104b47e56d250b6d5cff3d5b54ff4fbd3549
0xrgb <0xrgb@users.noreply.github.com>
2021-10-16 03:16:25 -0700

    Change board API to match announcement

--- OnlineJudge/board/views/oj.py
+++ OnlineJudge/board/views/oj.py
@@ -8,8 +8,11 @@ from ..serializers import ArticleSerializer, ArticleListSerializer
 from ..serializers import CreateArticleSerializer, RemoveArticleSerializer
 
 
-class BoardArticleAPI(APIView):
+class BoardAPI(APIView):
     def get(self, request):
+        """
+        get an article via id
+        """
         id = request.GET.get("id")
         if not id or not check_is_id(id):
             return self.error("Invalid parameter, id is required")
@@ -20,9 +23,48 @@ class BoardArticleAPI(APIView):
         data = ArticleSerializer(contest).data
         return self.success(data)
 
+    @validate_serializer(CreateArticleSerializer)
+    @login_required
+    def post(self, request):
+        """
+        write new article
+        """
+        data = request.data
+        article = Article.objects.create(
+            title=data["title"],
+            content=data["content"],
+            created_by=request.user,
+        )
+        return self.success({"id": article.id})
+
+    @validate_serializer(RemoveArticleSerializer)
+    @login_required
+    def delete(self, request):
+        """
+        remove an article
+        """
+        data = request.data
+        id = data["id"]
+        if not id or not check_is_id(id):
+            return self.error("Invalid parameter, id is required")
+        try:
+            article = Article.objects.get(id=id)
+        except Article.DoesNotExist:
+            return self.error("Article does not exist")
+
+        user = request.user
+        if user.is_admin_role() or user.id == article.created_by.id:
+            article.delete()
+            return self.success({"id": id})
+
+        return self.error("No permission to remove article")
+
 
 class BoardListAPI(APIView):
     def get(self, request):
+        """
+        get a list of articles
+        """
         problem_id = request.GET.get("problem_id")
         if not problem_id or problem_id.strip() == "":
             articles = Article.objects.all()
@@ -33,40 +75,4 @@ class BoardListAPI(APIView):
                 return self.error("Problem does not exist")
             articles = Article.objects.filter(problem=problem)
         return self.success(self.paginate_data(request, article, ArticleListSerializer))
-
-
-class BoardWriteAPI(APIView):
-    @validate_serializer(CreateArticleSerializer)
-    @login_required
-    def post(self, request):
-        data = request.data
-        article = Article.objects.create(
-            title=data["title"],
-            content=data["content"],
-            created_by=request.user,
-        )
-        return self.success({"id": article.id})
-
-
-class BoardRemoveAPI(APIView):
-    @validate_serializer(RemoveArticleSerializer)
-    @login_required
-    def post(self, request):
-        # TODO: check ID
-        data = request.data
-        id = data["id"]
-        if not id or not check_is_id(id):
-            return self.error("Invalid parameter, id is required")
-        try:
-            article = Article.objects.get(id=id)
-        except Article.DoesNotExist:
-            return self.error("Article does not exist")
-
-        # Check (user is admin) or (user is writer)
-        user = request.user
-        if user.is_authenticated and (user.is_admin_role() or user.id == article.created_by.id):
-            article.delete()
-            return self.success({"id": id})
-
-        return self.error("No permission to remove article")
 
